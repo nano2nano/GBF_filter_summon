bcad31603b45234c5e2aa475e77586158d558c44
nano <76194982+nano2nano@users.noreply.github.com>
2021-02-20 18:37:53 +0900

    Fix bug.

--- popup.js
+++ popup.js
@@ -7,7 +7,7 @@ auto_reload.addEventListener('change', function () {
     bg.localStorage.setItem("AUTO_RELOAD", this.checked);
 });
 
-send_notification_end_of_battle.checked = bg.localStorage.getItem("toggle_send_notification_end_of_battle") == "true";
+send_notification_end_of_battle.checked = bg.localStorage.getItem("SEND_NOTIFICATION_END_OF_BATTLE") == "true";
 send_notification_end_of_battle.addEventListener("change", function () {
     bg.localStorage.setItem("SEND_NOTIFICATION_END_OF_BATTLE", this.checked);
 });
